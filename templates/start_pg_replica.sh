2b408e6dc92a2d395c0b16efe2768f7311889255
Bas Meijer <bas.meijer@me.com>
2022-03-25 13:59:14 +0100

    updated

--- templates/start_pg_replica.sh
+++ templates/start_pg_replica.sh
@@ -3,22 +3,22 @@
 
 cd /tmp
 echo Stopping PostgreSQL
-service postgresql-{{ base_postgres_m }} stop
+systemctl stop {{ base_postgres_service }}
 
 echo Cleaning up old cluster directory
-su - postgres -c "rm -rf /var/lib/pgsql/{{ base_postgres_m }}/data/*"
+su - postgres -c "rm -rf {{ base_postgres_pgdata }}/*"
 
 echo Starting base backup as replicator
-su - postgres -c "pg_basebackup -w -h {{ base_postgres_mip }} -D /var/lib/pgsql/{{ base_postgres_m }}/data -X stream -U replicator -v -P"
+su - postgres -c "pg_basebackup -w -h {{ base_postgres_mip }} -D {{ base_postgres_pgdata }} -X stream -U replicator -v -P"
 
 echo Writing recovery.conf file
-su - postgres -c "cat > /var/lib/pgsql/{{ base_postgres_m }}/data/recovery.conf <<- EOF
+su - postgres -c "cat > {{ base_postgres_pgdata }}/recovery.conf <<- EOF
 standby_mode = 'on'
 primary_conninfo = 'host={{ base_postgres_mip }} port={{ base_postgres_port }} user=replicator password={{ base_postgres_pass }}'
 trigger_file = '/tmp/postgresql.trigger'
 EOF
 "
-chmod 600 /var/lib/pgsql/{{ base_postgres_m }}/data/recovery.conf
+chmod 600 {{ base_postgres_pgdata }}/recovery.conf
 
 echo Starting PostgreSQL
-service postgresql-{{ base_postgres_m }} start
+systemctl start {{ base_postgres_service }}
